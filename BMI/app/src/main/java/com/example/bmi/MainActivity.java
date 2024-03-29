aafa019eab878029d2833b4569a5bc8b63ae58ed
jimm433 <jim43621203@gmail.com>
2024-03-29 13:07:39 +0800

    0329

---
+++ BMI/app/src/main/java/com/example/bmi/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.bmi;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText edtHeight = findViewById(R.id.edtHeight);
+        final EditText edtWeight = findViewById(R.id.edtWeight);
+        Button btnCalc = findViewById(R.id.btnCalc);
+        Button btnClear = findViewById(R.id.btnClear);
+        final TextView txvShow = findViewById(R.id.txvShow);
+
+        btnCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String heightStr = edtHeight.getText().toString();
+                String weightStr = edtWeight.getText().toString();
+
+                // 檢查身高和體重是否已輸入
+                if ("".equals(heightStr) || "".equals(weightStr)) {
+                    txvShow.setText("請輸入數字"); // 提示請輸入數字
+                    txvShow.setTextColor(Color.BLACK); // 使用黑色顯示提示信息
+                    return; // 退出此方法
+                }
+
+                try {
+                    float height = Float.parseFloat(heightStr) / 100;
+                    float weight = Float.parseFloat(weightStr);
+                    float bmi = weight / (height * height);
+
+                    String resultText = "BMI: " + String.format("%.2f", bmi);
+                    txvShow.setText(resultText);
+
+                    if (bmi < 18.5) {
+                        txvShow.setTextColor(Color.BLUE);
+                    } else if (bmi > 24.9) {
+                        txvShow.setTextColor(Color.RED);
+                    } else {
+                        txvShow.setTextColor(Color.GREEN);
+                    }
+                } catch (NumberFormatException e) {
+                    txvShow.setText("輸入格式錯誤，請輸入數字");
+                    txvShow.setTextColor(Color.BLACK);
+                }
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edtHeight.setText("");
+                edtWeight.setText("");
+                txvShow.setText("");
+                txvShow.setTextColor(Color.BLACK);
+            }
+        });
+    }
+}
