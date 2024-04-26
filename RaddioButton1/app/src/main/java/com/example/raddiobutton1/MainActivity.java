7db2dd8a0686acf1d706f91cb3e50317ea0fe6d0
jimm433 <jim43621203@gmail.com>
2024-04-26 13:50:30 +0800

    0426

--- RaddioButton1/app/src/main/java/com/example/raddiobutton1/MainActivity.java
+++ RaddioButton1/app/src/main/java/com/example/raddiobutton1/MainActivity.java
@@ -1,4 +1,6 @@
+//A111222007
 package com.example.raddiobutton1;
+
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -6,12 +8,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import com.example.raddiobutton1.MainActivity2;
 
 public class MainActivity extends AppCompatActivity {
     public static String shoppingCartContent = "";
@@ -21,76 +27,104 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button buttonAddToCart = findViewById(R.id.Btn_addshopcar);
-        final Button buttonViewCart = findViewById(R.id.Btn_shopcar);
-        buttonAddToCart.setOnClickListener(new View.OnClickListener() {
+        final Button btnOK = findViewById(R.id.Btn_OK);
+        final RadioButton rdbBoy = findViewById(R.id.rdbBoy);
+        final RadioButton rdbGirl = findViewById(R.id.rdbGirl);
+        final RadioGroup rgType = findViewById(R.id.rgType);
+        final EditText ticketNumber = findViewById(R.id.Tickets);
+        final TextView lblOutput = findViewById(R.id.lblOutput);
+
+        //男生
+        rdbBoy.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                RadioButton rdbBoy = findViewById(R.id.rdbBoy);
-                RadioButton rdbGirl = findViewById(R.id.rdbGirl);
-                RadioGroup rgType = findViewById(R.id.rgType);
-                EditText ticketNumber = findViewById(R.id.Tickets);
-                TextView lblOutput = findViewById(R.id.lblOutput);
-
-                // 處理性別選擇
-                String gender = rdbBoy.isChecked() ? getResources().getString(R.string.male) : getResources().getString(R.string.female);
-
-                // 初始化價格和票種
-                int price = 0;
-                String ticketType = "";
-                int checkedId = rgType.getCheckedRadioButtonId();
-
-                // 判斷選中的票種
-                if (checkedId == R.id.rdbAdult) {
-                    price = 500;
-                    ticketType = getResources().getString(R.string.regularticket);
-                } else if (checkedId == R.id.rdbChild) {
-                    price = 400;
-                    ticketType = getResources().getString(R.string.childticket);
-                } else if (checkedId == R.id.rdbStudent) {
-                    price = 250;
-                    ticketType = getResources().getString(R.string.studentticket);
-                }
-
-                // 檢查票數是否為空
-                if (ticketNumber.getText().toString().isEmpty()) {
-                    new AlertDialog.Builder(MainActivity.this)
-                            .setMessage("請填寫票的張數")
-                            .setPositiveButton("確定", null)
-                            .show();
-                    return;
-                }
-
-                // 獲取票數並計算總價
-                int numberOfTickets = Integer.parseInt(ticketNumber.getText().toString());
-                int total = price * numberOfTickets;
-
-                // 構建輸出字符串
-                String outputStr = String.format("%s\n%s %d 張\n金額 %d 元", gender, ticketType, numberOfTickets, total);
-                lblOutput.setText(outputStr);
-
-                // 彈出確認對話框
-                new AlertDialog.Builder(MainActivity.this)
-                        .setTitle("加入購物車")
-                        .setMessage("是否將以下商品加入購物車？\n" + outputStr)
-                        .setPositiveButton("是", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                shoppingCartContent += outputStr + "\n";
-                            }
-                        })
-                        .setNegativeButton("否", null)
-                        .show();
+                updateTicketInfo(rdbBoy, rgType, ticketNumber, lblOutput);
             }
         });
+        //女生
+        rdbGirl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateTicketInfo(rdbBoy, rgType, ticketNumber, lblOutput);
+            }
+        });
+        //票種
+        rgType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                updateTicketInfo(rdbBoy, rgType, ticketNumber, lblOutput);
+            }
+        });
+        //張數
+        ticketNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
-        buttonViewCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                updateTicketInfo(rdbBoy, rgType, ticketNumber, lblOutput);
+            }
+        });
+        //確認按鈕
+        btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // 創建一個意圖以啟動 MainActivity2
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                // 把購物車內容作為字符串附加到意圖
-                intent.putExtra("cartContent", shoppingCartContent);
-                startActivity(intent);  // 啟動 MainActivity2
+                updateTicketInfo(rdbBoy, rgType, ticketNumber, lblOutput);
+
+                if (lblOutput.getText().toString().isEmpty() || lblOutput.getText().toString().contains("票的張數未填寫或格式不正確")) {
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("錯誤")
+                            .setMessage("請先填寫訊息確認！")
+                            .setPositiveButton("確定", null)
+                            .show();
+                } else {
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    intent.putExtra("PurchaseDetails", lblOutput.getText().toString());
+                    startActivity(intent);
+                }
             }
         });
+    }
+
+    //購票訊息
+    private void updateTicketInfo(RadioButton rdbBoy, RadioGroup rgType, EditText ticketNumber, TextView lblOutput) {
+        String gender = rdbBoy.isChecked() ? getResources().getString(R.string.male) : getResources().getString(R.string.female);
+        int price = 0;
+        String ticketType = "";
+        int total = 0;
+        int numberOfTickets = 0;
+        boolean validTickets = false;
+
+        try {
+            numberOfTickets = Integer.parseInt(ticketNumber.getText().toString());
+            validTickets = true;
+        } catch (NumberFormatException e) {
+            validTickets = false;
+        }
+
+        int checkedId = rgType.getCheckedRadioButtonId();
+        if (checkedId == R.id.rdbAdult) {
+            price = 500;
+            ticketType = getResources().getString(R.string.regularticket);
+        } else if (checkedId == R.id.rdbChild) {
+            price = 400;
+            ticketType = getResources().getString(R.string.childticket);
+        } else if (checkedId == R.id.rdbStudent) {
+            price = 250;
+            ticketType = getResources().getString(R.string.studentticket);
+        }
+
+        String outputStr = String.format("%s\n%s", gender, ticketType);
+        if (validTickets) {
+            total = price * numberOfTickets;
+            outputStr += String.format(" %d 張\n金額 %d 元", numberOfTickets, total);
+        } else {
+            outputStr += "\n票的張數未填寫或格式不正確";
+        }
+
+        lblOutput.setText(outputStr);
     }
 }
--- RaddioButton1/app/src/main/java/com/example/raddiobutton1/MainActivity2.java
+++ RaddioButton1/app/src/main/java/com/example/raddiobutton1/MainActivity2.java
@@ -1,13 +1,11 @@
 package com.example.raddiobutton1;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -17,52 +15,19 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        TextView shopThingLbl = findViewById(R.id.shopthinglbl);
-        Button backToShopBtn = findViewById(R.id.backtoshopbtn);
-        Button clearCarBtn = findViewById(R.id.clearcarbtn);
-        Button buyBtn = findViewById(R.id.buybtn);
 
         Intent intent = getIntent();
-        shopThingLbl.setText(MainActivity.shoppingCartContent);
+        String purchaseDetails = intent.getStringExtra("PurchaseDetails");
 
-        // 返回商店按鈕
-        backToShopBtn.setOnClickListener(v -> finish());
+        TextView shopThingLbl = findViewById(R.id.shopthinglbl);
+        shopThingLbl.setText(purchaseDetails);
 
-        // 清空購物車按鈕
-        clearCarBtn.setOnClickListener(v -> {
-            shopThingLbl.setText("");  // 清空 TextView 顯示
-            MainActivity.shoppingCartContent = "";  // 清空全局購物車內容
-        });
-
-        // 確認購買按鈕
-        buyBtn.setOnClickListener(v -> {
-            if (!MainActivity.shoppingCartContent.isEmpty()) {
-                new AlertDialog.Builder(MainActivity2.this)
-                        .setTitle("確認購買")
-                        .setMessage("您即將購買以下商品：\n" + MainActivity.shoppingCartContent)
-                        .setPositiveButton("確認", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                shopThingLbl.setText("");
-                                MainActivity.shoppingCartContent = "";
-                                showPurchaseSuccessDialog();
-                            }
-                        })
-                        .setNegativeButton("取消", null)
-                        .show();
-            } else {
-                new AlertDialog.Builder(MainActivity2.this)
-                        .setMessage("購物車為空")
-                        .setPositiveButton("確定", null)
-                        .show();
+        Button backToShopBtn = findViewById(R.id.backtoshopbtn);
+        backToShopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
-    }
-
-    private void showPurchaseSuccessDialog() {
-        new AlertDialog.Builder(MainActivity2.this)
-                .setMessage("購買成功！")
-                .setPositiveButton("確定", (dialog, which) -> finish())  // 購買成功後關閉此活動
-                .show();
     }
 }
